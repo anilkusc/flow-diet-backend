93fab2c20b049b69c16c20c802c0ca7c9b079efe
anilkusc <anilkuscu95@gmail.com>
2022-01-31 00:21:04 +0300

    feat: :art: added logging

--- api/routes.go
+++ api/routes.go
@@ -3,9 +3,30 @@ package api
 import (
 	"io"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 func (api *Api) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "returnValue")
+	return
+}
+func (api *Api) ReadUserHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// TODO add this process to the middleware
+	id, ok := vars["id"]
+	if !ok {
+		log.Error("id is missing in URI")
+	}
+	io.WriteString(w, id)
+	return
+}
+func (api *Api) UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "returnValue")
+	return
+}
+func (api *Api) DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "returnValue")
 	return
 }
--- app/app.go
+++ app/app.go
@@ -2,11 +2,12 @@ package app
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/anilkusc/flow-diet-backend/api"
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 // App method is the main struct for the application
@@ -18,10 +19,19 @@ type App struct {
 func (app *App) Init() {
 	app.API.Router = mux.NewRouter()
 	app.API.InitRoutes()
+	if os.Getenv("ENV") == "dev" {
+		log.SetLevel(log.DebugLevel)
+	} else if os.Getenv("ENV") == "stg" {
+		log.SetLevel(log.InfoLevel)
+	} else if os.Getenv("ENV") == "prod" {
+		log.SetLevel(log.WarnLevel)
+	} else {
+		log.SetLevel(log.TraceLevel)
+	}
 }
 
 func (app *App) Start() {
 	app.Init()
-	log.Println("Serving on: 8080")
+	log.Warn("Serving on: 8080")
 	log.Fatal(http.ListenAndServe(":8080", app.API.Router))
 }
